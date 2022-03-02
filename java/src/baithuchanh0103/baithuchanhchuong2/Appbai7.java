58a5af5ff2a74bac45c8ffa466de91886e4705e8
tranductrinh962 <97505817+tranductrinh962@users.noreply.github.com>
2022-03-02 14:18:27 +0700

    Delete java/src/baithuchanh0103 directory

--- java/src/baithuchanh0103/baithuchanhchuong2/Appbai7.java
+++
@@ -1,9 +0,0 @@
-package baithuchanh0103.baithuchanhchuong2;
-
-class Appbai7 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++) {
-            System.out.println(i);
-        }
-    }
-}
